d498d55879ce31215ce913600b58abf52c49ed79
Christian M <chriamue@gmail.com>
2020-05-07 08:17:55 +0200

    feat: export DP3TStatus

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { NativeModules } from 'react-native';
-import NewDiaryEntry from './components/NewDiaryEntry/index';
-import DP3T from './components/DP3T/index';
+import NewDiaryEntry from './components/NewDiaryEntry';
+import DP3T from './components/DP3T';
+import DP3TStatus from './components/DP3TStatus';
 
 type CDiaryRnType = {
   getDeviceName(): Promise<string>;
@@ -10,4 +11,4 @@ const { CDiaryRn } = NativeModules;
 
 export default CDiaryRn as CDiaryRnType;
 
-export { NewDiaryEntry, DP3T };
+export { NewDiaryEntry, DP3T, DP3TStatus };
